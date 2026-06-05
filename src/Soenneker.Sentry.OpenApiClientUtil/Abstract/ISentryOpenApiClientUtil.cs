497b971c906e038e55c1d9240c8f433912069656
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:44 -0500

    xml commenting

--- src/Soenneker.Sentry.OpenApiClientUtil/Abstract/ISentryOpenApiClientUtil.cs
+++ src/Soenneker.Sentry.OpenApiClientUtil/Abstract/ISentryOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Sentry.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ISentryOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<SentryOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
